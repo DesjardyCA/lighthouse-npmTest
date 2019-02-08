6e0f3e8bf8204abf401663985fec149c3213f6bb
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-08 11:22:03 -0800

    testing chalk usage in index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var chalk = require("chalk");
 
-var message = "Hello " + chalk.yellow("World");
+var message = `${chalk.blue('Hello')} ${chalk.red.bgWhite('World')}`
 console.log(message);
+
+console.log(chalk.green(
+  'I am a green line ' +
+  chalk.blue.underline.bold('with a blue substring') +
+  ' that becomes green again!'
+));
